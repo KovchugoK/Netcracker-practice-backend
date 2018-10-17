63ec0d5a055c834f5b7c2fd4d30b92d51a04000a
Sanek Gyy <Demchenko.sasha@mail.ru>
2018-10-18 00:23:10 +0300

    fix repository

--- src/main/java/com/netcrackerpractice/startup_social_network/repository/ResumeRepository.java
+++ src/main/java/com/netcrackerpractice/startup_social_network/repository/ResumeRepository.java
@@ -2,6 +2,8 @@ package com.netcrackerpractice.startup_social_network.repository;
 
 import com.netcrackerpractice.startup_social_network.entity.Resume;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
 }
--- src/main/java/com/netcrackerpractice/startup_social_network/repository/StartupRepository.java
+++ src/main/java/com/netcrackerpractice/startup_social_network/repository/StartupRepository.java
@@ -2,7 +2,9 @@ package com.netcrackerpractice.startup_social_network.repository;
 
 import com.netcrackerpractice.startup_social_network.entity.Startup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StartupRepository extends JpaRepository<Startup, Long> {
 
 }
