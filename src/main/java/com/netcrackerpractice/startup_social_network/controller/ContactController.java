a8169b12498e345cbaae79ee8033aa3afee27ac9
Yauheni <bugatti1701@gmail.com>
2018-11-05 01:38:26 +0300

    Added contact controller

---
+++ src/main/java/com/netcrackerpractice/startup_social_network/controller/ContactController.java
@@ -0,0 +1,26 @@
+package com.netcrackerpractice.startup_social_network.controller;
+
+import com.netcrackerpractice.startup_social_network.entity.Account;
+import com.netcrackerpractice.startup_social_network.service.AccountService;
+import com.netcrackerpractice.startup_social_network.service.impl.ContactServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactController {
+    @Autowired
+    ContactServiceImpl contactService;
+
+    @Autowired
+    AccountService accountService;
+
+    @GetMapping("/{userId}")
+    public List<Account> getUserContacts(@PathVariable(name = "userId") long userId,
+                                         @RequestParam(name = "name", required = false) String name) {
+        return contactService.getUserContactsAccounts(userId, name);
+    }
+
+}
