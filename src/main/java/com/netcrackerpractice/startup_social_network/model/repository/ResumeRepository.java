0989ab48e2db6b46c6d581991c121948d059d44a
Egor Ananko <egorananko@gmail.com>
2018-11-08 16:33:08 +0300

    Remove wrong placed files.

--- src/main/java/com/netcrackerpractice/startup_social_network/model/repository/ResumeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.netcrackerpractice.startup_social_network.model.repository;
-
-import com.netcrackerpractice.startup_social_network.entity.Resume;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResumeRepository extends JpaRepository<Resume, Long> {
-
-}
