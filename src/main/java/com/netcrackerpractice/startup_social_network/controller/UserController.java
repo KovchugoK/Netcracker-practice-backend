b47b3e4616a5f2f2095c8937160e6d6c5fddafa1
Demchenko Alexander <demchenko.sasha@mail.ru>
2018-11-26 21:22:22 +0300

    Finish imageloader for main accountpage

--- src/main/java/com/netcrackerpractice/startup_social_network/controller/UserController.java
+++ src/main/java/com/netcrackerpractice/startup_social_network/controller/UserController.java
@@ -1,10 +1,9 @@
 package com.netcrackerpractice.startup_social_network.controller;
 
 
-import com.netcrackerpractice.startup_social_network.entity.Role;
+import com.netcrackerpractice.startup_social_network.entity.Account;
 import com.netcrackerpractice.startup_social_network.entity.User;
-import com.netcrackerpractice.startup_social_network.entity.enums.RoleEnum;
-import com.netcrackerpractice.startup_social_network.repository.RoleRepository;
+import com.netcrackerpractice.startup_social_network.service.AccountService;
 import com.netcrackerpractice.startup_social_network.service.ImageService;
 import com.netcrackerpractice.startup_social_network.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -27,22 +25,29 @@ public class UserController {
     @Autowired
     ImageService imageService;
 
+    @Autowired
+    AccountService accountService;
+
     @GetMapping("/user-list")
     public List<User> getStartupList() {
         return userService.findAll();
     }
 
     @PostMapping("/image")
-    private ResponseEntity<?> saveImage(@RequestParam("image") MultipartFile image,
+    private ResponseEntity<?> saveImages(@RequestParam("image") MultipartFile image,
                                              @RequestParam(name = "Id") UUID accountId) {
-
-
         try {
-            imageService.saveImage(image, accountId);
+            Optional<Account> accountOptional = accountService.findAccountById(accountId);
+
+            if (accountOptional.isPresent()) {
+                Account account = accountOptional.get();
+                accountService.saveImages(image, account);
+            } else
+                return  ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("FAIL to upload. Did't find account");
+
             return ResponseEntity.status(HttpStatus.OK).body("You successfully uploaded");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("FAIL to upload");
         }
-
     }
 }
--- src/main/java/com/netcrackerpractice/startup_social_network/service/impl/AccountServiceImpl.java
+++ src/main/java/com/netcrackerpractice/startup_social_network/service/impl/AccountServiceImpl.java
@@ -3,9 +3,14 @@ package com.netcrackerpractice.startup_social_network.service.impl;
 import com.netcrackerpractice.startup_social_network.entity.Account;
 import com.netcrackerpractice.startup_social_network.repository.AccountRepository;
 import com.netcrackerpractice.startup_social_network.service.AccountService;
+import com.netcrackerpractice.startup_social_network.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -15,6 +20,9 @@ public class AccountServiceImpl implements AccountService {
 
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    ImageService imageService;
 
     @Override
     public Account saveAccount(Account account) {
@@ -29,5 +37,24 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public List<Account> findAllAccounts() {
         return accountRepository.findAll();
+    }
+
+    @Override
+    public void saveImages(MultipartFile image, Account account) throws IOException, GeneralSecurityException {
+
+            File imageFile = imageService.convertMultipartToFile(image);
+            String imageId = imageService.saveImageToGoogleDrive(imageFile);
+
+            String comressedImagePath = imageService.compressionImage(imageFile);
+            File comressedImageFile = new File(comressedImagePath);
+            String comressedImageId = imageService.saveImageToGoogleDrive(comressedImageFile);
+
+            account.setImageId(imageId);
+            account.setCompressedImageId(comressedImageId);
+            accountRepository.save(account);
+
+            imageFile.delete();
+            comressedImageFile.delete();
+
     }
 }
