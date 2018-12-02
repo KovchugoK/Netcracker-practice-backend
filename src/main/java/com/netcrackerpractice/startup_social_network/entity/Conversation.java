031a311cb5c05abc2675610b32ba6ef08fe29b00
Yauheni Harkavik <bugatti1701@gmail.com>
2018-12-03 01:58:15 +0300

    Added entity for conversation

---
+++ src/main/java/com/netcrackerpractice/startup_social_network/entity/Conversation.java
@@ -0,0 +1,34 @@
+package com.netcrackerpractice.startup_social_network.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "conversation")
+public class Conversation {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_your_account", nullable = false)
+    private Account yourAccount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_other_account", nullable = false)
+    private Account otherAccount;
+
+    private String name;
+}
