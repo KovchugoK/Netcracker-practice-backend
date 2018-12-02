dc3eecfc9e1de9d1726e0274bb103e787e3718e3
ElizavetaBuzun <buzun.elizaveta@gmail.com>
2018-12-02 18:37:24 +0300

    remove image processing from UserController to AccountController

--- src/main/java/com/netcrackerpractice/startup_social_network/controller/UserController.java
+++ src/main/java/com/netcrackerpractice/startup_social_network/controller/UserController.java
@@ -1,29 +1,21 @@
 package com.netcrackerpractice.startup_social_network.controller;
 
 
-import com.netcrackerpractice.startup_social_network.entity.Account;
 import com.netcrackerpractice.startup_social_network.entity.User;
 import com.netcrackerpractice.startup_social_network.service.AccountService;
-import com.netcrackerpractice.startup_social_network.service.ImageService;
 import com.netcrackerpractice.startup_social_network.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("api/user")
 public class UserController {
     @Autowired
     UserService userService;
-
-    @Autowired
-    ImageService imageService;
 
     @Autowired
     AccountService accountService;
@@ -33,21 +25,4 @@ public class UserController {
         return userService.findAll();
     }
 
-    @PostMapping("/image")
-    private ResponseEntity<?> saveImages(@RequestParam("image") MultipartFile image,
-                                             @RequestParam(name = "Id") UUID accountId) {
-        try {
-            Optional<Account> accountOptional = accountService.findAccountById(accountId);
-
-            if (accountOptional.isPresent()) {
-                Account account = accountOptional.get();
-                accountService.saveImages(image, account);
-            } else
-                return  ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("FAIL to upload. Did't find account");
-
-            return ResponseEntity.status(HttpStatus.OK).body("You successfully uploaded");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("FAIL to upload");
-        }
-    }
 }
