bae7119478605b8a4b22a628c67106fb3b694f40
Yauheni Harkavik <bugatti1701@gmail.com>
2018-12-06 03:01:14 +0300

    Changed conversation repository and service: added getConversationIdByUsersIds

--- src/main/java/com/netcrackerpractice/startup_social_network/repository/ConversationRepository.java
+++ src/main/java/com/netcrackerpractice/startup_social_network/repository/ConversationRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ConversationRepository extends JpaRepository<Conversation, UUID> {
     @Query(value = "SELECT * FROM conversation c WHERE c.id_your_account=:userId", nativeQuery = true)
     List<Conversation> getUserConversationsById(@Param("userId") UUID userId);
+
+    @Query(value = "SELECT * FROM conversation c WHERE c.id_your_account=:yourId AND c.id_other_account=:otherId", nativeQuery = true)
+    Optional<Conversation> getConversationIdByUsersIds(@Param("yourId") UUID yourId, @Param("otherId") UUID otherId);
 }
--- src/main/java/com/netcrackerpractice/startup_social_network/service/ConversationService.java
+++ src/main/java/com/netcrackerpractice/startup_social_network/service/ConversationService.java
@@ -1,10 +1,14 @@
 package com.netcrackerpractice.startup_social_network.service;
 
 import com.netcrackerpractice.startup_social_network.entity.Conversation;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ConversationService {
     List<Conversation> getUserConversationsById(UUID userId);
+
+    Optional<Conversation> getConversationIdByUsersIds(UUID yourId, UUID otherId);
 }
