0dccd85e2236a673c27f08865ea92ac549ec9ed0
Yauheni Harkavik <bugatti1701@gmail.com>
2018-12-08 22:30:44 +0300

    Updated conversation controller

--- src/main/java/com/netcrackerpractice/startup_social_network/controller/ConversationController.java
+++ src/main/java/com/netcrackerpractice/startup_social_network/controller/ConversationController.java
@@ -5,11 +5,9 @@ import com.netcrackerpractice.startup_social_network.exception.ConversationNotFo
 import com.netcrackerpractice.startup_social_network.mapper.ConversationMapper;
 import com.netcrackerpractice.startup_social_network.service.ConversationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -26,5 +24,10 @@ public class ConversationController {
                 conversationService.getConversationByUsersIds(yourId, otherId).orElseThrow(
                         () -> new ConversationNotFoundException("Conversation not found for users: " + yourId + "and " + otherId)
                 ));
+    }
+
+    @GetMapping("/{userId}")
+    public List<ConversationDTO> getConversationInfoById(@PathVariable(name = "userId") UUID userId) {
+        return conversationMapper.conversationDTOtoConversation(conversationService.getUserConversationsById(userId));
     }
 }
